16bcf7ab8bdfd1ae1d95008add626b76acaa8e50
Gavin Day <mngad@leeds.ac.uk>
2018-05-30 14:47:26 +0100

    scatters for intact human

--- Bar/bar_human_1_2.py
+++ Bar/bar_human_1_2.py
@@ -28,63 +28,63 @@ VB13 = df['G41-11 L5']
 
 fig, ax = plt.subplots()
 index = np.arange(n_groups)
-novb = 8.0
+novb = 14.0
 bar_width = 0.8/novb
 opacity = 0.8
 
-#rects1 = plt.bar(index, VB0, bar_width,
-#                 alpha=opacity,
-#                 color='b',
-#                 label='Spine 1 L1')
-#rects2 = plt.bar(index + (bar_width) , VB1, bar_width,
-#                 alpha=opacity,
-#                 color='g',
-#                 label='Spine 1 L2')
-#rects3 = plt.bar(index+ (bar_width *2), VB2, bar_width,
-#                 alpha=opacity,
-#                 color='r',
-#                 label='Spine 1 L3')
-#rects4 = plt.bar(index + (bar_width *3), VB3, bar_width,
-#                 alpha=opacity,
-#                 color='c',
-#                 label='Spine 1 L4')
-#rects5 = plt.bar(index + (bar_width *4), VB4, bar_width,
-#                 alpha=opacity,
-#                 color='m',
-#                 label='Spine 1 L5')
-#rects6 = plt.bar(index+ (bar_width *5), VB5, bar_width,
-#                 alpha=opacity,
-#                 color='y',
-#                 label='Spine 2 L1')
-rects7 = plt.bar(index + (bar_width*0) , VB6, bar_width,
+rects1 = plt.bar(index, VB0, bar_width,
+                 alpha=opacity,
+                 color='b',
+                 label='Spine 1 L1')
+rects2 = plt.bar(index + (bar_width) , VB1, bar_width,
+                 alpha=opacity,
+                 color='g',
+                 label='Spine 1 L2')
+rects3 = plt.bar(index+ (bar_width *2), VB2, bar_width,
+                 alpha=opacity,
+                 color='r',
+                 label='Spine 1 L3')
+rects4 = plt.bar(index + (bar_width *3), VB3, bar_width,
+                 alpha=opacity,
+                 color='c',
+                 label='Spine 1 L4')
+rects5 = plt.bar(index + (bar_width *4), VB4, bar_width,
+                 alpha=opacity,
+                 color='m',
+                 label='Spine 1 L5')
+rects6 = plt.bar(index+ (bar_width *5), VB5, bar_width,
+                 alpha=opacity,
+                 color='y',
+                 label='Spine 2 L1')
+rects7 = plt.bar(index + (bar_width*6) , VB6, bar_width,
                  alpha=opacity,
                  color='k',
                  label='Spine 3 L1')
-rects8 = plt.bar(index+ (bar_width *1), VB7, bar_width,
+rects8 = plt.bar(index+ (bar_width *7), VB7, bar_width,
                  alpha=opacity,
                  color='xkcd:purple',
                  label='Spine 3 L2')
-rects9 = plt.bar(index + (bar_width *2), VB8, bar_width,
+rects9 = plt.bar(index + (bar_width *8), VB8, bar_width,
                  alpha=opacity,
                  color='xkcd:teal',
                  label='Spine 3 L3')
-rects10 = plt.bar(index + (bar_width *3), VB9, bar_width,
+rects10 = plt.bar(index + (bar_width *9), VB9, bar_width,
                  alpha=opacity,
                  color='xkcd:olive',
                  label='Spine 4 L1')
-rects11 = plt.bar(index+ (bar_width *4), VB10, bar_width,
+rects11 = plt.bar(index+ (bar_width *10), VB10, bar_width,
                  alpha=opacity,
                  color='xkcd:salmon',
                  label='Spine 4 L2')
-rects12 = plt.bar(index + (bar_width*5) , VB11, bar_width,
+rects12 = plt.bar(index + (bar_width*11) , VB11, bar_width,
                  alpha=opacity,
                  color='xkcd:periwinkle',
                  label='Spine 4 L3')
-rects13 = plt.bar(index+ (bar_width *6), VB12, bar_width,
+rects13 = plt.bar(index+ (bar_width *12), VB12, bar_width,
                  alpha=opacity,
                  color='xkcd:turquoise',
                  label='Spine 4 L4')
-rects14 = plt.bar(index + (bar_width *7), VB13, bar_width,
+rects14 = plt.bar(index + (bar_width *13), VB13, bar_width,
                  alpha=opacity,
                  color='xkcd:mustard',
                  label='Spine 4 L5')
@@ -93,11 +93,11 @@ plt.xlabel('Loading Position')
 plt.ylabel('Change in Stiffness (%)')
 plt.xticks(index + bar_width)
 ax.set_xticklabels( df['Position'])
-plt.legend(fontsize=10)
+plt.legend(fontsize=6,bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 plt.tight_layout()
 #plt.show()
-plt.savefig('hum_male_1_2.png', dpi=None, facecolor='w', edgecolor='w',
+plt.savefig('hum_1_2.png', dpi=None, facecolor='w', edgecolor='w',
         orientation='landscape', papertype=None, format=None,
         transparent=False, bbox_inches=None, pad_inches=1,
         frameon=None)
